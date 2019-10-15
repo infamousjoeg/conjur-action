20b4abfa7ff05be6d2f80c3bc8d0d23a124f850f
Joe Garcia <joe@joe-garcia.com>
2019-10-15 14:45:15 -0400

    entrypoint to funcs

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,76 +19,59 @@ urlencode() {
     LC_COLLATE=$old_lc_collate
 }
 
+create_pem() {
+    # Create conjur_account.pem for valid SSL
+    echo "$INPUT_CERTIFICATE" > conjur_"$INPUT_ACCOUNT".pem
+}
+
+conjur_authn() {
+    if [[ -n "$INPUT_CERTIFICATE" ]]; then
+        # Authenticate and receive session token from Conjur - encode Base64
+        token=$(curl --cacert conjur_"$INPUT_ACCOUNT".pem --data "$INPUT_API_KEY" "$INPUT_URL"/authn/"$INPUT_ACCOUNT"/"$hostId"/authenticate | base64 | tr -d '\r\n')
+    else
+        # Authenticate and receive session token from Conjur - encode Base64
+        token=$(curl -k --data "$INPUT_API_KEY" "$INPUT_URL"/authn/"$INPUT_ACCOUNT"/"$hostId"/authenticate | base64 | tr -d '\r\n')
+    fi
+}
+
+array_secrets() {
+    IFS=';'
+    read -ra SECRETS <<< "$INPUT_SECRETS" # [0]=db/sqlusername | sql_username [1]=db/sql_password
+}
+
+set_secrets() {
+    for secret in "${SECRETS[@]}"; do
+        IFS='|'
+        read -ra METADATA <<< "$secret" # [0]=db/sqlusername [1]=sql_username
+
+        if [[ "${#METADATA[@]}" == 2 ]]; then
+            secretId=$(urlencode "${METADATA[0]}")
+            envVar=${METADATA[1]^^}
+        else
+            secretId=${METADATA[0]}
+            IFS='/'
+            read -ra SPLITSECRET <<< "$secretId" # [0]=db [1]=sql_password
+            arrLength=${#SPLITSECRET[@]} # Get array length
+            lastIndex=$((arrLength-1)) # Subtract one for last index
+            envVar=${SPLITSECRET[$lastIndex]^^}
+        fi
+
+        if [[ -n "$INPUT_CERTIFICATE" ]]; then
+            secretVal=$(curl --cacert conjur_"$INPUT_ACCOUNT".pem -H "Authorization: Token token=\"$token\"" "$INPUT_URL"/secrets/"$INPUT_ACCOUNT"/variable/"$secretId")
+        else
+            secretVal=$(curl -k -H "Authorization: Token token=\"$token\"" "$INPUT_URL"/secrets/"$INPUT_ACCOUNT"/variable/"$secretId")
+        fi
+
+        echo ::add-mask::"${secretVal}" # Masks the value in all logs & output
+        echo ::set-env name="${envVar}"::"${secretVal}" # Set environment variable
+    done
+}
+
 # URL-encode Host ID for future use
 hostId=$(urlencode "$INPUT_HOST_ID")
 
-# If certificate input is not empty...
-if [[ -n "$INPUT_CERTIFICATE" ]]; then
-    # Create conjur_account.pem for valid SSL
-    echo "$INPUT_CERTIFICATE" > conjur_"$INPUT_ACCOUNT".pem
-
-    # Authenticate and receive session token from Conjur - encode Base64
-    token=$(curl --cacert conjur_"$INPUT_ACCOUNT".pem --data "$INPUT_API_KEY" "$INPUT_URL"/authn/"$INPUT_ACCOUNT"/"$hostId"/authenticate | base64 | tr -d '\r\n')
-
-    # Iterate through secrets parsing...
-    # Secrets Example: db/sqlusername | sql_username; db/sql_password
-    IFS=';'
-    read -ra SECRETS <<< "$INPUT_SECRETS" # [0]=db/sqlusername | sql_username [1]=db/sql_password
-
-    for secret in "${SECRETS[@]}"; do
-        IFS='|'
-        read -ra METADATA <<< "$secret" # [0]=db/sqlusername [1]=sql_username
-
-        if [[ "${#METADATA[@]}" == 2 ]]; then
-            secretId=$(urlencode "${METADATA[0]}")
-            envVar=${METADATA[1]^^}
-        else
-            secretId=${METADATA[0]}
-            IFS='/'
-            read -ra SPLITSECRET <<< "$secretId" # [0]=db [1]=sql_password
-            arrLength=${#SPLITSECRET[@]} # Get array length
-            lastIndex=$((arrLength-1)) # Subtract one for last index
-            envVar=${SPLITSECRET[$lastIndex]^^}
-        fi
-
-        secretVal=$(curl --cacert conjur_"$INPUT_ACCOUNT".pem -H "Authorization: Token token=\"$token\"" "$INPUT_URL"/secrets/"$INPUT_ACCOUNT"/variable/"$secretId")
-
-        echo ::add-mask::"${secretVal}" # Masks the value in all logs & output
-        echo ::set-env name="${envVar}"::"${secretVal}" # Set environment variable
-    done
-
-    IFS=' '
-# Else certificate input is empty...
-else
-    # Authenticate and receive session token from Conjur - encode Base64
-    token=$(curl -k --data "$INPUT_API_KEY" "$INPUT_URL"/authn/"$INPUT_ACCOUNT"/"$hostId"/authenticate | base64 | tr -d '\r\n')
-
-    # Iterate through secrets after parsing...
-    # Secrets Example: db/sqlusername | sql_username; db/sql_password
-    IFS=';'
-    read -ra SECRETS <<< "$INPUT_SECRETS" # [0]=db/sqlusername | sql_username [1]=db/sql_password
-
-    for secret in "${SECRETS[@]}"; do
-        IFS='|'
-        read -ra METADATA <<< "$secret" # [0]=db/sqlusername [1]=sql_username
-
-        if [[ "${#METADATA[@]}" == 2 ]]; then
-            secretId=$(urlencode "${METADATA[0]}")
-            envVar=${METADATA[1]^^}
-        else
-            secretId=${METADATA[0]}
-            IFS='/'
-            read -ra SPLITSECRET <<< "$secretId" # [0]=db [1]=sql_password
-            arrLength=${#SPLITSECRET[@]} # Get array length
-            lastIndex=$((arrLength-1)) # Subtract one for last index
-            envVar=${SPLITSECRET[$lastIndex]^^}
-        fi
-
-        secretVal=$(curl -k -H "Authorization: Token token=\"$token\"" "$INPUT_URL"/secrets/"$INPUT_ACCOUNT"/variable/"$secretId")
-
-        echo ::add-mask::"${secretVal}" # Masks the value in all logs & output
-        echo ::set-env name="${envVar}"::"${secretVal}" # Set environment variable
-    done
-
-    IFS=' '
-fi
+create_pem()
+conjur_authn()
+# Secrets Example: db/sqlusername | sql_username; db/sql_password
+array_secrets()
+set_secrets()
